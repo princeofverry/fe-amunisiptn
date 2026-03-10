dbc947f38a667fae91b8d0d44d75c7b1a3eef228
Finodya Yahdun <finodyayahdun@gmail.com>
2026-03-10 17:01:29 +0700

    add register page

---
+++ src/components/organisms/auth/AuthRegisterWrapper.tsx
@@ -0,0 +1,9 @@
+import CardAuthRegister from "@/components/molecules/card/auth/CardAuthRegister";
+
+export default function AuthRegisterWrapper() {
+  return (
+    <section className="min-h-screen bg-main bg-cover bg-bottom bg-no-repeat flex items-center justify-center p-4">
+      <CardAuthRegister />
+    </section>
+  );
+}
